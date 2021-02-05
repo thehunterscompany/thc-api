7d92134e179f789033a753a884caab3d6c5dca7e
Armando Zubillaga <ajzpiv97@gmail.com>
2021-02-04 23:32:54 -0500

    Created extra constant

--- app/utils/contants.py
+++ app/utils/contants.py
@@ -2,4 +2,6 @@ import os
 
 DATABASE_NAME = os.getenv('DATABASE_NAME', 'thc')
 DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
+TEST_DATABASE_NAME = os.getenv('TEST_DATABASE_NAME', 'thc_test')
 DATABASE_PORT = int(os.getenv('DATABASE_PORT', 27017))
+

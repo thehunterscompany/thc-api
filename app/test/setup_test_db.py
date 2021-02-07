0d44df1744494b1c344300eb2195db6b848c536e
Armando Zubillaga <ajzpiv97@gmail.com>
2021-02-07 12:10:42 -0500

    Modified class to only setup test db

--- app/test/setup_test_db.py
+++ app/test/setup_test_db.py
@@ -1,27 +1,24 @@
-import unittest
-
 import pymongo
 
 from app import create_app
 from app.utils.contants import *
 
 
-class SetupTestDB(unittest.TestCase):
+class SetupTestDB:
+    @staticmethod
+    def start_db():
+        my_client = pymongo.MongoClient("mongodb://localhost:27017/")
+        db_list = my_client.list_database_names()
+        if TEST_DATABASE_NAME in db_list:
+            my_client.drop_database(TEST_DATABASE_NAME)
+        test_db = my_client[TEST_DATABASE_NAME]
+        app = create_app(testing=True)
 
-    my_client = pymongo.MongoClient("mongodb://localhost:27017/")
-    app = create_app(testing=True)
-    app_client = app.test_client()
+        app_client = app.test_client()
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.db_list = cls.my_client.list_database_names()
-        if TEST_DATABASE_NAME in cls.db_list:
-            cls.my_client.drop_database(TEST_DATABASE_NAME)
-        cls.test_db = cls.my_client[TEST_DATABASE_NAME]
-        cls.app = create_app(testing=True)
+        return test_db, app_client
 
-        cls.app_client = cls.app.test_client()
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        cls.my_client.drop_database(TEST_DATABASE_NAME)
+    @staticmethod
+    def close_db():
+        my_client = pymongo.MongoClient("mongodb://localhost:27017/")
+        my_client.drop_database(TEST_DATABASE_NAME)

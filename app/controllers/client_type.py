ab1aadcaea0f18ae6603f1392d7903b0a7e78d87
Armando Zubillaga <ajzpiv97@gmail.com>
2021-02-09 16:21:10 -0500

    Moved post methods

---
+++ app/controllers/client_type.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, request, abort, jsonify
+from mongoengine import NotUniqueError
+
+from ..collections.client_type import ClientTypes
+from ..collections.role import Roles
+from ..utils import response
+
+bp = Blueprint('client_types', __name__, url_prefix='/')
+
+
+@bp.route('/client_types', methods=['POST'])
+def post_client_type():
+    """
+    Post New Client Type
+    """
+    if request.content_type == 'application/json':
+        data = request.get_json()
+    else:
+        data = request.form
+    try:
+        client_type = ClientTypes(employment_type=data['employment_type'],
+                                  documents=None)
+        client_type.save()
+        return jsonify({'result': client_type.format()})
+
+    except NotUniqueError:
+        abort(422)
+
+    except Exception:
+        abort(404)

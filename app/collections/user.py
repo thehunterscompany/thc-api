826b16cfa4e508719682c01d43a6dde4f467fbce
Armando Zubillaga <ajzpiv97@gmail.com>
2021-02-15 14:20:59 -0500

    Added arg

--- app/collections/user.py
+++ app/collections/user.py
@@ -6,9 +6,9 @@ from app.collections.role import Roles
 
 
 class Users(me.Document):
-    email = me.EmailField(unique=True)
-    password = me.StringField()
-    role_type = me.ReferenceField(document_type=Roles, reverse_delete_rule=me.CASCADE)
+    email = me.EmailField(unique=True, required=True)
+    password = me.StringField(required=True)
+    role_type = me.ReferenceField(document_type=Roles, reverse_delete_rule=me.CASCADE, required=True)
     verified = me.BooleanField(default=False)
 
     meta = {'allow_inheritance': True}

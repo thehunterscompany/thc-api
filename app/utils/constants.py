020ee49e8f43882a46f49b588336d02ba0c774ea
Armando Zubillaga <ajzpiv97@gmail.com>
2021-02-15 18:54:22 -0500

    Removed data

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -7,7 +7,8 @@ DATABASE_PORT = int(os.getenv('DATABASE_PORT', 27017))
 
 MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
 MAIL_PORT = int(os.getenv('MAIL_PORT', 465))
-MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'ajzpiv97@gmail.com')
-MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', '220567Lp')
+MAIL_USERNAME = os.getenv('MAIL_USERNAME', '')
+MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', '')
 MAIL_USE_SSL = bool(os.getenv('MAIL_USE_SSL', True))
+
 

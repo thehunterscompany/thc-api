99273a0d844006a3619ff19d06152e1eba9edee6
Armando Zubillaga <ajzpiv97@gmail.com>
2021-02-15 14:49:41 -0500

    Created schema

---
+++ app/schemas/client_schema.py
@@ -0,0 +1,12 @@
+from marshmallow import Schema, fields
+
+from app.collections.profile import Profiles
+
+
+class SaveClientInput(Schema):
+    email = fields.Str(required=True)
+    password = fields.Str(required=True)
+    role_type = fields.Raw(required=True)
+    profiles = fields.Raw(required=True)
+    credit_line = fields.Raw(required=True)
+

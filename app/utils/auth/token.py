aefa41426ec6a571d8317180ec874c0f4bf3094e
Armando Zubillaga <ajzpiv97@gmail.com>
2021-02-15 22:12:47 -0500

    Fixed bug

--- app/utils/auth/token.py
+++ app/utils/auth/token.py
@@ -22,11 +22,11 @@ def confirm_token(token, expiration=3600) -> str or bool:
     :param expiration:
     :return
     """
-    serializer = URLSafeTimedSerializer(current_app.config[SECRET_KEY])
+    serializer = URLSafeTimedSerializer(SECRET_KEY)
     try:
         email = serializer.loads(
             token,
-            salt=current_app.config[SECURITY_PASSWORD_SALT],
+            salt=SECURITY_PASSWORD_SALT,
             max_age=expiration
         )
     except:

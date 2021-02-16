9d718d24597fb4803c334201755d05a00f8d5e69
Armando Zubillaga <ajzpiv97@gmail.com>
2021-02-15 19:02:14 -0500

    Added security keys

--- app/utils/config/email.py
+++ app/utils/config/email.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import current_app
 from flask_mail import Mail, Message
 
@@ -14,10 +16,12 @@ def setup_email(app):
     app.config['MAIL_USERNAME'] = MAIL_USERNAME
     app.config['MAIL_PASSWORD'] = MAIL_PASSWORD
     app.config['MAIL_USE_SSL'] = MAIL_USE_SSL
+    app.config['SECRET_KEY'] = SECURITY_PASSWORD_SALT
+    app.config['SECURITY_PASSWORD_SALT'] = SECURITY_PASSWORD_SALT
 
 
 def send_mail():
     mail = Mail(current_app)
     msg = Message('Hello', sender='ajzpiv97@gmail.com', recipients=['ajzpiv97@gmail.com'])
-    msg.body = "Hello Flask message sent from Flask-Mail"
+    msg.body = "Hello Flask message sent from Flask-Mail {}".format(datetime.datetime.now())
     mail.send(msg)
--- app/utils/constants.py
+++ app/utils/constants.py
@@ -11,4 +11,5 @@ MAIL_USERNAME = os.getenv('MAIL_USERNAME', '')
 MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', '')
 MAIL_USE_SSL = bool(os.getenv('MAIL_USE_SSL', True))
 
-
+SECRET_KEY = os.getenv('SECRET_KEY', '')
+SECURITY_PASSWORD_SALT = os.getenv('SECURITY_PASSWORD_SALT', '')

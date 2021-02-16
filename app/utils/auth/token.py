ea40eb3f7034105e399079186f362040b73805a4
Armando Zubillaga <ajzpiv97@gmail.com>
2021-02-15 19:36:09 -0500

    Created function for token

---
+++ app/utils/auth/token.py
@@ -0,0 +1,34 @@
+from itsdangerous import URLSafeTimedSerializer
+
+from flask import current_app
+
+from app.utils.constants import SECRET_KEY, SECURITY_PASSWORD_SALT
+
+
+def generate_confirmation_token(email: str) -> URLSafeTimedSerializer:
+    """
+
+    :param email:
+    :return:
+    """
+    serializer = URLSafeTimedSerializer(current_app.config[SECRET_KEY])
+    return serializer.dumps(email, salt=current_app.config[SECURITY_PASSWORD_SALT])
+
+
+def confirm_token(token, expiration=3600) -> str or bool:
+    """
+
+    :param token:
+    :param expiration:
+    :return
+    """
+    serializer = URLSafeTimedSerializer(current_app.config[SECRET_KEY])
+    try:
+        email = serializer.loads(
+            token,
+            salt=current_app.config[SECURITY_PASSWORD_SALT],
+            max_age=expiration
+        )
+    except:
+        return False
+    return email

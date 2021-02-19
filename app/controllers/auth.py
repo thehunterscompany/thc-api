bbdf06f77f6c17d17d3e2b5ea6204ca4db84bfa0
Armando Zubillaga <ajzpiv97@gmail.com>
2021-02-19 00:51:23 -0500

    Test driven dev for auth endpoints

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -72,28 +72,35 @@ def register():
 
         new_user_instance = Clients(**client_instance).save()
 
-        token = generate_confirmation_token(new_user_instance.email)
-        # return '{}'.format(token)
-        confirm_url = url_for('auth.confirm_email', token=token, _external=True)
-        html = render_template('send_confirmation.html', confirm_url=confirm_url)
-        subject = "Please confirm your email"
-        send_mail(new_user_instance.email, html, subject)
-
-        return response(parser_one_object(new_user_instance)), 201
-
     except NotUniqueError as err:
-        new_user_instance.delete()
         credit_line_instance.delete()
         for profile in profiles:
             profile.delete()
         rewrite_abort(422, err)
 
     except Exception as err:
-        new_user_instance.delete()
         credit_line_instance.delete()
         for profile in profiles:
             profile.delete()
         rewrite_abort(400, err)
+
+    try:
+        token = generate_confirmation_token(new_user_instance.email)
+
+        confirm_url = url_for('auth.confirm_email', token=token, _external=True)
+        html = render_template('send_confirmation.html', confirm_url=confirm_url)
+        subject = "Please confirm your email"
+
+        send_mail(new_user_instance.email, html, subject)
+
+        return response(parser_one_object(new_user_instance)), 201
+
+    except Exception as err:
+        new_user_instance.delete()
+        credit_line_instance.delete()
+        for profile in profiles:
+            profile.delete()
+        rewrite_abort(422, err)
 
 
 @bp.route('/confirm/<token>', methods=['GET'])
@@ -139,20 +146,27 @@ def login():
     """
     User login
     """
-
     try:
-        data = request.json
+        user = Users.objects(email=request.json['email']).first()
+        if user is None:
+            raise AuthError({'code': 'Not Found',
+                                     'description': 'User was not'
+                                                    ' found in our database'
+                             }, 404)
+    except AuthError as err:
+        rewrite_abort(err.status_code, err.error['description'])
 
-        user = Users.objects.get(
-            email=data['email']
-        )
-
+    else:
         if user.verified:
-            if data['password'] == decrypt_data(user.password):
-                return response(generate_jwt(user))
-            else:
-                err = 'Your password is incorrect.'
-                rewrite_abort(401, err)
-
-    except Exception as err:
-        rewrite_abort(422, err)
+            try:
+                if request.json['password'] == decrypt_data(user.password):
+                    return response(generate_jwt(user))
+                else:
+                    raise AuthError({'code': 'Unauthorized',
+                                     'description': 'Your password is incorrect!'
+                                     }, 401)
+            except AuthError as err:
+                rewrite_abort(err.status_code, err.error['description'])
+        else:
+            err = 'User has not confirm registration! Please check email.'
+            rewrite_abort(403, err)
--- app/test/controllers/auth/test_auth.py
+++ app/test/controllers/auth/test_auth.py
@@ -10,12 +10,14 @@ class AuthTestCase(DefaultSetup):
     os.environ['SECURITY_PASSWORD_SALT'] = 'salt'
 
     def tearDown(self):
+        self.app_client.application.config["MAIL_DEFAULT_SENDER"] = None
         active_col = self.test_db.list_collection_names()
         for collection in active_col:
             if collection in ('users', 'profiles', 'credit_lines'):
                 self.test_db[collection].drop()
 
     def test_register_login_client(self):
+        self.app_client.application.config["MAIL_DEFAULT_SENDER"] = 'ajzpiv97@gmail.com'
         payload = {"email": "ajzpiv97@gmail.com",
                    "password": "12345",
                    "role_type": "client",
@@ -83,103 +85,162 @@ class AuthTestCase(DefaultSetup):
         res = self.app_client.get('/confirm/{}'.format(token))
         self.assertEqual(res.status_code, 200)
 
-    # def test_fail_register_client(self):
-    #     # Not unique id
-    #     payload = {"email": "ajzpiv97@gmail.com",
-    #                "password": "12345",
-    #                "role_type": "client",
-    #                "profiles": [
-    #                    {
-    #                        "name": "Armando",
-    #                        "last_name": "Zubillaga",
-    #                        "age": 25,
-    #                        "personal_id": "1111",
-    #                        "income": "12314423",
-    #                        "client_type": "employee"
-    #
-    #                    },
-    #                    {
-    #                        "name": "Jose",
-    #                        "last_name": "Prado",
-    #                        "age": 29,
-    #                        "personal_id": "1111",
-    #                        "income": "12314423",
-    #                        "client_type": "employee"
-    #                    }],
-    #                "number_owners": 2,
-    #                "credit_line": {"budget": "124324",
-    #                                "initial_payment": "41234123423",
-    #                                "financing_value": "14324123413242314",
-    #                                "credit_line_type": "leasing",
-    #                                "financing_time": "1232"},
-    #                }
-    #
-    #     res = self.app_client.post('/register', json=payload)
-    #     data = json.loads(res.data)
-    #     self.assertEqual(res.status_code, 422)
-    #     self.assertTrue(data['result'], 'unprocessable')
-    #
-    #     # Missing value
-    #     payload = {"email": "ajzpiv98@gmail.com",
-    #                "password": "12345",
-    #                "profiles": [
-    #                    {
-    #                        "name": "Armando",
-    #                        "last_name": "Zubillaga",
-    #                        "age": 25,
-    #                        "personal_id": "113241311",
-    #                        "income": "12314423",
-    #                        "client_type": "employee"
-    #
-    #                    },
-    #                    {
-    #                        "name": "Jose",
-    #                        "last_name": "Prado",
-    #                        "age": 29,
-    #                        "personal_id": "11524654611",
-    #                        "income": "12314423",
-    #                        "client_type": "employee"
-    #                    }],
-    #                "number_owners": 2,
-    #                "credit_line": {"budget": "124324",
-    #                                "initial_payment": "41234123423",
-    #                                "financing_value": "14324123413242314",
-    #                                "credit_line_type": "leasing",
-    #                                "financing_time": "1232"},
-    #                }
-    #
-    #     res = self.app_client.post('/register', json=payload)
-    #     data = json.loads(res.data)
-    #     self.assertEqual(res.status_code, 400)
-    #     self.assertTrue(data['result'], 'bad request')
-    #
-    #     payload = {"email": "ajzpiv97@gmail.com",
-    #                "password": "12345",
-    #                "role_type": "client",
-    #                "profiles": [
-    #                    {
-    #                        "name": "Armando",
-    #                        "last_name": "Zubillaga",
-    #                        "age": 25,
-    #                        "personal_id": "111534532453451",
-    #                        "income": "12314423",
-    #                        "client_type": "employee"
-    #
-    #                    }],
-    #                "number_owners": 1,
-    #                "credit_line": {"budget": "124324",
-    #                                "initial_payment": "41234123423",
-    #                                "financing_value": "14324123413242314",
-    #                                "credit_line_type": "leasing",
-    #                                "financing_time": "1232"},
-    #                }
-    #
-    #     res = self.app_client.post('/register', json=payload)
-    #     data = json.loads(res.data)
-    #     self.assertEqual(res.status_code, 422)
-    #     self.assertTrue(data['result'], 'unprocessable')
+    def test_fail_register_client_failed_email(self):
+        # Fail to send email
+        payload = {"email": "ajzpiv97@gmail.com",
+                   "password": "12345",
+                   "role_type": "client",
+                   "profiles": [
+                       {
+                           "name": "Armando",
+                           "last_name": "Zubillaga",
+                           "age": 25,
+                           "personal_id": "1111",
+                           "income": "12314423",
+                           "client_type": "employee"
 
-    # def test_email_confirmation(self):
-    #     res = self.app_client.get('/confirm/{}'.format(
-    #         "ImFqenBpdjk3QGdt""YWlsLmNvbSI.YCs-Dg.bSZDcEVvK832qLkTa22V7ZJ8oi4"))
-    #     self.assertEqual(res.status_code, 422)
+                       },
+                       {
+                           "name": "Jose",
+                           "last_name": "Prado",
+                           "age": 29,
+                           "personal_id": "115411",
+                           "income": "12314423",
+                           "client_type": "employee"
+                       }],
+                   "number_owners": 2,
+                   "credit_line": {"budget": "124324",
+                                   "initial_payment": "41234123423",
+                                   "financing_value": "14324123413242314",
+                                   "credit_line_type": "leasing",
+                                   "financing_time": "1232"},
+                   }
+
+        res = self.app_client.post('/register', json=payload)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 422)
+        self.assertTrue(data['result'], 'unprocessable')
+
+        # Missing value
+        payload = {"email": "ajzpiv98@gmail.com",
+                   "password": "12345",
+                   "profiles": [
+                       {
+                           "name": "Armando",
+                           "last_name": "Zubillaga",
+                           "age": 25,
+                           "income": "12314423",
+                           "client_type": "employee"
+
+                       },
+                       {
+                           "name": "Jose",
+                           "last_name": "Prado",
+                           "age": 29,
+                           "personal_id": "11524654611",
+                           "income": "12314423",
+                           "client_type": "employee"
+                       }],
+                   "number_owners": 2,
+                   "credit_line": {"budget": "124324",
+                                   "initial_payment": "41234123423",
+                                   "financing_value": "14324123413242314",
+                                   "credit_line_type": "leasing",
+                                   "financing_time": "1232"},
+                   }
+
+        res = self.app_client.post('/register', json=payload)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 400)
+        self.assertTrue(data['result'], 'bad request')
+
+    def test_fail_login(self):
+        self.app_client.application.config["MAIL_DEFAULT_SENDER"] = 'ajzpiv97@gmail.com'
+
+        # Wrong password
+        payload = {"email": "ajzpiv97@gmail.com",
+                   "password": "12345",
+                   "role_type": "client",
+                   "profiles": [
+                       {
+                           "name": "Armando",
+                           "last_name": "Zubillaga",
+                           "age": 25,
+                           "personal_id": "1111",
+                           "income": "12314423",
+                           "client_type": "employee"
+
+                       },
+                       {
+                           "name": "Jose",
+                           "last_name": "Prado",
+                           "age": 29,
+                           "personal_id": "115411",
+                           "income": "12314423",
+                           "client_type": "employee"
+                       }],
+                   "number_owners": 2,
+                   "credit_line": {"budget": "124324",
+                                   "initial_payment": "41234123423",
+                                   "financing_value": "14324123413242314",
+                                   "credit_line_type": "leasing",
+                                   "financing_time": "1232"},
+                   "verified": True
+                   }
+        self.app_client.post('/register', json=payload)
+
+        payload = {'email': 'ajzpiv97@gmail.com',
+                   'password': '123456'}
+
+        res = self.app_client.post('/login', json=payload)
+        self.assertEqual(res.status_code, 401)
+
+        # User not found
+        payload = {'email': 'ajz@gmail.com',
+                   'password': '123456'}
+
+        res = self.app_client.post('/login', json=payload)
+        self.assertEqual(res.status_code, 404)
+
+    def test_fail_login_user_not_verified(self):
+        self.app_client.application.config["MAIL_DEFAULT_SENDER"] = 'ajzpiv97@gmail.com'
+
+        # Wrong password
+        payload = {"email": "ajzpiv97@gmail.com",
+                   "password": "12345",
+                   "role_type": "client",
+                   "profiles": [
+                       {
+                           "name": "Armando",
+                           "last_name": "Zubillaga",
+                           "age": 25,
+                           "personal_id": "1111",
+                           "income": "12314423",
+                           "client_type": "employee"
+
+                       },
+                       {
+                           "name": "Jose",
+                           "last_name": "Prado",
+                           "age": 29,
+                           "personal_id": "115411",
+                           "income": "12314423",
+                           "client_type": "employee"
+                       }],
+                   "number_owners": 2,
+                   "credit_line": {"budget": "124324",
+                                   "initial_payment": "41234123423",
+                                   "financing_value": "14324123413242314",
+                                   "credit_line_type": "leasing",
+                                   "financing_time": "1232"},
+                   }
+        self.app_client.post('/register', json=payload)
+
+        payload = {'email': 'ajzpiv97@gmail.com',
+                   'password': '123456'}
+
+        res = self.app_client.post('/login', json=payload)
+        self.assertEqual(res.status_code, 403)
+
+
+

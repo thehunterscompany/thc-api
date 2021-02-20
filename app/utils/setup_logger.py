c35ac5871b48a66c8300fdf4ef4b5b3883c85c2e
Armando Zubillaga <ajzpiv97@gmail.com>
2021-02-20 00:51:24 -0500

    Setup logger

---
+++ app/utils/setup_logger.py
@@ -0,0 +1,9 @@
+import logging
+
+
+def init_logger(log_file_path: str = None):
+    logging.basicConfig(filename=log_file_path, level=logging.INFO,
+                        format="[%(asctime)s] p%(process)s {%(filename)s:"
+                               "%(lineno)d} (levelname)s - %(message)s")
+    logger = logging.getLogger(__name__)
+    return logger

ec315c64f8967a6e811a561c5065cb9851b4be3b
Armando Zubillaga <ajzpiv97@gmail.com>
2021-02-20 15:19:43 -0500

    Modified document

--- app/collections/client.py
+++ app/collections/client.py
@@ -5,7 +5,7 @@ from app.collections.user import Users
 
 class Clients(Users):
     # profiles = me.ListField(me.ReferenceField(document_type=Profiles, reverse_delete_rule=me.NULLIFY), required=True)
-    # credit_line = me.ReferenceField(document_type=CreditLines, reverse_delete_rule=me.NULLIFY, required=True)
+    credit_line = me.ReferenceField('CreditLines')
     number_owners = me.IntField()
     referred_by = me.StringField()
 
--- app/schemas/client_schema.py
+++ app/schemas/client_schema.py
@@ -1,11 +1,10 @@
 from marshmallow import Schema, fields
 
-from app.collections.profile import Profiles
-
 
 class SaveClientInput(Schema):
     email = fields.Str(required=True)
     password = fields.Str(required=True)
     role_type = fields.Raw(required=True)
     verified = fields.Bool()
-
+    referred_by = fields.Str()
+    credit_line = fields.Raw()

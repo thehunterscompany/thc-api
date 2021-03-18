e8874dfb623fd247040f78248948419d3afc5fe8
Armando Zubillaga <ajzpiv97@gmail.com>
2021-03-18 00:35:45 -0400

    Added nested schema

--- app/schemas/client_schema.py
+++ app/schemas/client_schema.py
@@ -1,11 +1,11 @@
 from marshmallow import Schema, fields
 
+from app.schemas.user_schema import SaveUserInput
+
 
 class SaveClientInput(Schema):
-    email = fields.Str(required=True)
-    password = fields.Str(required=True)
-    role_type = fields.Raw(required=True)
-    verified = fields.Bool()
-    referred_by = fields.Str()
-    credit_line = fields.Raw()
-    profiles = fields.Raw()
+    user = fields.Nested(SaveUserInput)
+    referred_by_non_related = fields.Str()
+    referred_by_client = fields.Str()
+
+

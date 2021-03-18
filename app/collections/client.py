efa76905f911bc4e5839dac057004e16b81dd668
Armando Zubillaga <ajzpiv97@gmail.com>
2021-03-18 00:35:31 -0400

    Added extra fields and modified kwrargs

--- app/collections/client.py
+++ app/collections/client.py
@@ -5,8 +5,9 @@ from app.collections.user import Users
 
 class Clients(me.Document):
     user = me.ReferenceField(document_type=Users, reverse_delete_rule=me.CASCADE, required=True)
-    profiles = me.ListField(me.ReferenceField('Profiles'))
-    credit_line = me.ReferenceField('CreditLines')
-    number_owners = me.IntField()
-    referred_by = me.StringField()
+    profiles = me.ListField(me.ReferenceField('Profiles'), null=True)
+    credit_line = me.ReferenceField('CreditLines', null=True)
+    referred_by_non_related = me.StringField()
+    referred_by_client = me.ReferenceField('Clients', null=True)
+
 

41716afb44ac6db0bbd5523c13d110533e7bdf37
Armando Zubillaga <ajzpiv97@gmail.com>
2021-03-22 14:26:50 -0400

    Changed to field type

--- app/schemas/client_schema.py
+++ app/schemas/client_schema.py
@@ -1,10 +1,8 @@
 from marshmallow import Schema, fields
 
-from app.schemas.user_schema import SaveUserInput
-
 
 class SaveClientInput(Schema):
-    user = fields.Nested(SaveUserInput)
+    user = fields.Raw()
     referred_by_non_related = fields.Str()
     referred_by_client = fields.Str()
 
